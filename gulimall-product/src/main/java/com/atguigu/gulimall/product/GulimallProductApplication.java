3dec0b529f1593a9dfe567287441372c7d4baf05
hugh98 <822051253@qq.com>
2022-05-28 15:52:15 +0800

    基础环境搭建

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
@@ -1,9 +1,19 @@
 package com.atguigu.gulimall.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 1. 整合Mybatis-Plus
+ *      1)、导入依赖
+ *      2）、配置
+ *          1. 配置数据源
+ *              导入数据库的驱动
+ *          2. 配置Mybatis-Plus
+ */
 @SpringBootApplication
+@MapperScan("com.atguigu.gulimall.product.dao")
 public class GulimallProductApplication {
 
     public static void main(String[] args) {
